52652e6c116bc1ea9cf0c687c343383a6c0133b1
Wendell Viani <wendellylan@gmail.com>
2026-04-12 19:17:49 -0300

    Atualizado método get_lines

--- backend/graphic_data.py
+++ backend/graphic_data.py
@@ -1,6 +1,5 @@
 from .lp_solver import get_result
 import re
-import matplotlib.pyplot as mpl
 
 def get_lines():
     result = get_result()
@@ -8,24 +7,25 @@ def get_lines():
     restrictions = result["restrictions"] # Ex: ["3*x+2*y<=90", ...]
     
     lines = []
-    for restriction, k in restrictions:
+    for k, restriction in enumerate(restrictions):
         equation = re.sub(r"<=|>=|<|>", "=", restriction)  # Ex: "3*x+2*y=90"
         left_side, right_side = equation.split("=")
         right_side = float(right_side)
 
-        # x=0, solve for y
-        y = eval(left_side, {}, {variables[0]: 0, variables[1]: 1})
-        y = right_side / y  # 90 / 2 = 45.0
-
-        # set y=0, solve for x
         x = eval(left_side, {}, {variables[0]: 1, variables[1]: 0})
-        x = right_side / x  # 90 / 3 = 30.0
+        y = eval(left_side, {}, {variables[0]: 0, variables[1]: 1})
 
-        lines.append({
-            k+1: {
-                variables[0]: x,  # "x": 30.0
-                variables[1]: y   # "y": 45.0
-            }
-        })
+        if y != 0 and x != 0:
+            x_pts = [0.0, right_side / x]
+            y_pts = [right_side / y, 0.0]
+        elif y == 0:
+            x_val = right_side / x
+            x_pts = [x_val, x_val]
+            y_pts = [0.0, right_side]
+        else:
+            y_val = right_side / y
+            x_pts = [0.0, right_side]
+            y_pts = [y_val, y_val]
 
+        lines.append((x_pts, y_pts))
     return lines
