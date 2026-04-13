dab8374ef58f23ea86f12e77b83d7f61e56f0e30
Walker Viani <walkeryslan@gmail.com>
2026-04-13 15:20:31 -0300

    adiciona o icone do aplicativo na barra de tarefas

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
-from PySide6.QtWidgets import QApplication
 import sys
-
+import ctypes
+from PySide6.QtWidgets import QApplication
+from PySide6.QtGui import QIcon
 from ui.main_window import MainWindow
 
 def main():
+
+    # Tells Windows to treat this as its own app, separate from Python
+    if sys.platform == "win32":
+        ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(
+            "linear-programming-calculation"
+        )
+
     app = QApplication(sys.argv)
+
+    # Sets the icon globally for all windows in the app
+    app.setWindowIcon(QIcon("assets/icon.ico"))
 
     window = MainWindow()
     window.show()
